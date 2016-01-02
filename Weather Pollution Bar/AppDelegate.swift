ec203c079a1652d7f6728b48fcf3e23b57af57f8
Madhur <madhur.ahuja@helpchat.in>
2016-01-03 00:37:16 +0530

    Initial Commit

---
+++ Weather Pollution Bar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Weather Pollution Bar
+//
+//  Created by Madhur Ahuja on 03 Jan.
+//  Copyright © 2016 Madhur Ahuja. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
