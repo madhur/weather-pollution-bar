75d8323184c7280f41a34df060e204d0de63a62d
Madhur <madhur.ahuja@target.com>
2019-04-07 16:57:30 +0530

    Update files

--- Weather Pollution BarTests/Weather_Pollution_BarTests.swift
+++ Weather Pollution BarTests/Weather_Pollution_BarTests.swift
@@ -28,7 +28,7 @@ class Weather_Pollution_BarTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
